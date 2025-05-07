ad2ff10b3c538038cdd3047cca9d81690c58df2b
mohan2722002 <chmskrishna@gmail.com>
2025-05-08 02:56:30 +0530

    java second commit

--- HelloStudent.java
+++ HelloStudent.java
@@ -2,5 +2,6 @@ public class HelloStudent {
     public static void main(String[] args) {
         String name = "Gokul & Mohan";
         System.out.println("Hello, " + name + "!");
+	System.out.println("Welcome to CDAC")
     }
 }
